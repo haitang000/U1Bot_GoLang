50632347e5dc713dcf92664f1f592f848f6c63bb
haitang000 <108558839+haitang000@users.noreply.github.com>
2024-06-10 11:58:35 +0800

    🌈 添加疯狂星期四、今天吃什么功能

--- main.go
+++ main.go
@@ -29,6 +29,12 @@ func main(){
 	if input == "关于" {
 		about()
 	}
+	if input == "今天吃什么" {
+		whateat()
+	}
+	if input == "疯狂星期四" {
+		crazy_thursday()
+	}
 	if input == "go run main.go" {
 		fmt.Print("笨蛋！这都能输成运行命令！")
 		time.Sleep(time.Second * 5)
@@ -60,7 +66,7 @@ func fish(){
 	fmt.Println(" ")
 	rand.Seed(time.Now().UnixNano())
 	// 这玩楞就是根据目前时间来整的随机种子
-	var fishid = rand.Intn(19)
+	var fishid = rand.Intn(23)
 	// fishid:种类ID(从0开始，在加入/减少鱼的种类时这里也要修改)
 	var fishlong = rand.Intn(600)
 	// fishlong:鱼的长度(默认600)
@@ -160,6 +166,26 @@ func fish(){
 			danwei = "台"
 			fishlong = rand.Intn(20) + 20
 		}
+		if fishid == 20 {
+			fish = "烤激光鱼"
+			fishlong = rand.Intn(4000) + 200
+			// 尊重原作
+		}
+		if fishid == 21 {
+			fish = "虚空珍珠"
+			danwei = "颗"
+			fishlong = rand.Intn(3000) + 20
+			// 尊重原作
+		}
+		if fishid == 22 {
+			fish = "金蛙"
+			danwei = "只"
+			fishlong = rand.Intn(700) + 30
+		}
+		if fishid == 23 {
+			fish = "丁真鱼"
+			fishlong = rand.Intn(1000) + 10
+		}
 		//判断鱼的种类，到时候慢慢加
 	fmt.Print("\n正在钓鱼...")
 	time.Sleep(2 * time.Second)
@@ -168,7 +194,7 @@ func fish(){
 	var back string
 	fmt.Print("\n\n输入 “返回” 以返回主菜单")
 	fmt.Scanln(&back)
-	// 返回的代码我以前一直是搞反的，以至于我一时半会我还没发现这个BUG
+	// 返回的代码我以前一直是搞反的，以至于我一时半会我还没发现这个BUG qwq
 	if back == "返回" {
 		main()
 	}
@@ -187,6 +213,7 @@ func yunshi(){
 	if luck < 40 {
 		zhufuyu = "还好啦...还好啦..."
 	}
+	// :(
 	fmt.Print("\n你今天的幸运值是", luck, ",", zhufuyu)
 	var back string
 	fmt.Scanln(&back)
@@ -196,12 +223,155 @@ func yunshi(){
 	}
 }
 
+func whateat(){
+	fmt.Print("\n姚奕正在给你找好吃的...")
+	time.Sleep(2 * time.Second)
+	rand.Seed(time.Now().UnixNano())
+	// 这玩楞也是根据目前时间来整的随机种子
+	var something2eat = rand.Intn(29)
+	var eat string = "冰清玉洁丸"
+	// 夹带私货（
+	if something2eat == 0 {
+		eat = "小笼包"
+	}
+	if something2eat == 1 {
+		eat = "馄饨"
+	}
+	if something2eat == 2 {
+		eat = "馒头"
+	}
+	if something2eat == 3 {
+		eat = "冰淇淋"
+	}
+	if something2eat == 4 {
+		eat = "汉堡"
+	}
+	if something2eat == 5 {
+		eat = "炸鸡"
+	}
+	if something2eat == 6 {
+		eat = "肉夹馍"
+	}
+	if something2eat == 7 {
+		eat = "蔬菜沙拉"
+	}
+	if something2eat == 8 {
+		eat = "猪脚饭"
+	}
+	if something2eat == 9 {
+		eat = "烤肉"
+	}
+	if something2eat == 10 {
+		eat = "烤鱼"
+	}
+	if something2eat == 11 {
+		eat = "米饭"
+	}
+	if something2eat == 12 {
+		eat = "烤鸡"
+	}
+	if something2eat == 13 {
+		eat = "火锅"
+	}
+	if something2eat == 14 {
+		eat = "北京烤鸭"
+	}
+	if something2eat == 15 {
+		eat = "烤串"
+	}
+	if something2eat == 16 {
+		eat = "铁板烧"
+	}
+	if something2eat == 17 {
+		eat = "蒸鱼"
+	}
+	if something2eat == 18 {
+		eat = "红烧肉"
+	}
+	if something2eat == 19 {
+		eat = "薯片"
+	}
+	if something2eat == 20 {
+		eat = "关东煮"
+	}
+	if something2eat == 21 {
+		eat = "三明治"
+	}
+	if something2eat == 22 {
+		eat = "羊排"
+	}
+	if something2eat == 23 {
+		eat = "重庆小面"
+	}
+	if something2eat == 24 {
+		eat = "牛肉焗米粉"
+	}
+	if something2eat == 25 {
+		eat = "鱼香肉丝"
+	}
+	if something2eat == 26 {
+		eat = "葱油拌面"
+	}
+	if something2eat == 27 {
+		eat = "麻婆豆腐"
+	}
+	if something2eat == 28 {
+		eat = "火腿鹿茸菇炒荷兰豆"
+	}
+	if something2eat == 29 {
+		eat = "烤肉拌饭"
+	}
+	fmt.Print("\n姚奕推荐你吃", eat)
+	var back string
+	fmt.Scanln(&back)
+	fmt.Print("\n输入 “返回” 以返回主菜单")
+	if back == "返回" {
+		main()
+	}
+}
+
+func crazy_thursday(){
+	fmt.Print("\n姚奕正在给你找V50文案...")
+	time.Sleep(2 * time.Second)
+	rand.Seed(time.Now().UnixNano())
+	// 这玩楞也是根据目前时间来整的随机种子
+	var vivo50num = rand.Intn(4)
+	var vivo50 string = "V50文案"
+	if vivo50num == 0 {
+		vivo50 = "实不相瞒我结婚七年了，他不爱我也不放过我，孩子从小被H集团幕后掌托人也就是他爷爷奶奶带走，所有人都阻拦我见孩子，一场车祸我失去了所有记忆，只记得我好想他，夜不能寐，茶饭不思，寝食难安，重复做着我带着他去吃肯德基疯狂星期四的梦，所以能请我吃肯德基疯狂星期四帮我恢复丢失的记忆吗……"
+	}
+	if vivo50num == 1 {
+		vivo50 = "跟爸妈聊了很多，差不多年后就结婚，挺突然的吧，说真的，从来就没想过我这么快结婚，但现在我想收心了，遇到一个合适的人不容易，我希望我们举行婚礼的时候，所有好朋友都可以来参加，我知道我们都能好好照顾彼此，听到这个消息不知道你会不会伤心难过，会不会祝福，然而就算你不祝福我，也没事，明天肯德基疯狂星期四v我50请我吃"
+	}
+	if vivo50num == 2 {
+		vivo50 = "今天和家人聊了很多，最终还是同意妥协了，答应跟上个月相亲男孩交往，顺利的话差不多年底就结婚，说真的 从来就没想过我这么快结婚，但现在我想收心了不想玩了，虽然还不能说很了解彼此，但我希望我们举行婚礼的时候，所有好朋友都可以来参加，听到这个消息不知道有些人会不会伤心难过、会不会祝福，然而就算你不祝福我也没事、毕竟我是复制过来玩玩的，我刚读别人的文案的时候我也信了。但是今天是肯德基疯狂星期四！谁请我吃？"
+	}
+	if vivo50num == 3 {
+		vivo50 = "插播一条广告：\nApex双锤上大师\n清空赛季通行证\n免费帮上分\n身法教学\now80胜率上4300\n三位置意识教学\n彩虹六号生涯kd3.0\n包上冠军\nCSGO上全球精英\n这些都不接\n接肯德基疯狂星期四代吃"
+	}
+	if vivo50num == 4 {
+		vivo50 = "扒个群里的渣女，我有个朋友被群友睡了，大概是三个月以前吧，我朋友在群里认识了一个女的，他们在网上聊了差不多两个星期左右就见面了，而且第一次见面就去宾馆了 ，之后的每个星期六星期天都会去，就这样持续了好几个月，我朋友给那个女的说，已经一个多月没奖励自己了，想不到这句话说完，隔天就找不到那个女的了，手机一直打不通，关机，直到我朋友前几天跟她父母坦白这件事情，我朋友就在旁边一直哭，那天晚上我偷偷给他买了个石原里美同款，还是没奖励自己了，然后我第二天请假一天,专门给他聊这个事，他说那个女的对他很好很好什么的，我说对你好还不是想玩你，实在不行抱树吧，他当时听完这句话就趴桌子上大哭，正当我打开手机想抱树的时候，不小心打开了肯德基，想起来今天原来是肯德基疯狂星期四，所以说谁请我肯德基？吃完我继续说"
+	}
+	fmt.Print("\n", vivo50)
+	var back string
+	fmt.Scanln(&back)
+	fmt.Print("\n输入 “返回” 以返回主菜单")
+	if back == "返回" {
+		main()
+	}
+}
 func about(){
 	fmt.Print("\n=== U1Bot GoLang 重制版 ===")
-	fmt.Print("\n当前版本: V0.0.4 Alpha")
+	fmt.Print("\n当前版本: V0.1 Beta")
 	fmt.Print("\n原作者: Mr.ling")
 	fmt.Print("\n重制版作者: haitang000")
 	fmt.Print("\n原作GitHub仓库: https://github.com/crashvibe/u1bot/")
 	fmt.Print("\n原作官方Wiki: https://u1.tblstudio.cn/")
 	fmt.Print("\n原作官方QQ体验群: 713478803")
+	var back string
+	fmt.Scanln(&back)
+	fmt.Print("\n\n输入 “返回” 以返回主菜单")
+	if back == "返回" {
+		main()
+	}
 }
